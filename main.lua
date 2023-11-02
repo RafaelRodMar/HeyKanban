fdf3c2018efe44607321760e90e6a234fb8e60e1
Rafael Rodríguez Martínez <48961409+RafaelRodMar@users.noreply.github.com>
2023-11-02 18:29:07 +0100

    first commit

---
+++ main.lua
@@ -0,0 +1,52 @@
+function love.load()
+    --variables
+    gameWidth = 640
+    gameHeight = 480
+    love.window.setMode(gameWidth, gameHeight, {resizable=false, vsync=false})
+    love.graphics.setBackgroundColor(1,1,1) --white
+
+    --load font
+    font = love.graphics.newFont("sansation.ttf",25)
+    love.graphics.setFont(font)
+
+	-- Sprite that holds all imagery
+    -- imagedata = love.image.newImageData('isometric_demo.png')
+	-- sprIsom = love.graphics.newImage(imagedata)
+    -- textureWidth = imagedata:getWidth()
+    -- textureHeight = imagedata:getHeight()
+
+    -- mouse
+    mouseClicked = false
+    vMouse = {x=0, y=0}
+end
+
+function love.keypressed(key)
+end
+
+function love.keyreleased(key)
+end
+
+function love.mousemoved( x, y, dx, dy, istouch )
+    vMouse.x = x
+    vMouse.y = y
+end
+
+function love.mousepressed(x,y,button, istouch)
+	if button == 1 then
+		mouseClicked = true
+	end
+end
+
+function love.update(dt)
+    
+    mouseClicked = false
+end
+
+function love.draw()
+    love.graphics.setBackgroundColor(1,1,1)
+    love.graphics.setColor(1,1,1)
+
+    -- Draw Debug Info
+    --draw UI
+    love.graphics.setColor(1,0,0)
+end
